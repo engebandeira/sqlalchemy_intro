4d20ebbb89a17760a99abbc6fdb5aa1c78633390
animasouza <animasouza@gmail.com>
2025-06-27 09:43:44 -0300

    alembic_live_211

--- models.py
+++ models.py
@@ -2,7 +2,6 @@ from sqlalchemy import Integer, String, Column
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
-
 class Base(DeclarativeBase):
     pass
 
@@ -10,7 +9,7 @@ class Base(DeclarativeBase):
 class Pessoa(Base):
     __tablename__ = 'pessoa'
 
-    id = Column(Integer, primary_key=True)
-    nome= Column(String(50), nullable=False)
+    id    = Column(Integer, primary_key=True)
+    nome  = Column(String(50), nullable=False)
     email = Column(String(50), nullable=False)
     senha = Column(String(50), nullable=False)
